eae847447d7efc7be91709415af01d7e73130a8d
WINSTEN COELLINS <62231201@wil.local>
2024-05-31 16:04:09 +0700

    add:navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Navigation from "@/components/navigation";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/navigation.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+import home from '@/public/svgs/home.svg'
+import cart from '@/public/svgs/cart.svg'
+import profile from '@/public/svgs/profile.svg'
+import Link from "next/link"
+
+export default function Navigation() {
+    return (
+        <div className="flex justify-around bg-[#201F20] absolute -bottom-5 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 mx-auto py-3 rounded-3xl">
+            <div>
+                <Link href='/' className="flex flex-col items-center">
+                    <Image src={home} height={30} width={30} alt='icon' />
+                    <span className="text-white mt-1">Home</span>
+                </Link>
+            </div>
+            <div>
+                <Link href='/cart' className="flex flex-col items-center">
+                    <Image src={cart} height={30} width={30} alt='icon' /> 
+                    <span className="text-white mt-1">Cart</span>
+                </Link>
+            </div>
+            <div>
+                <Link href='/profile' className="flex flex-col items-center">
+                    <Image src={profile} height={30} width={30} alt='icon' />
+                    <span className="text-white mt-1">Profile</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
