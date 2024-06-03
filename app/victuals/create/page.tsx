3f9ffcd9d725ffd6d8e4349e53dd9063c77bfec3
WINSTEN COELLINS <62231201@wil.local>
2024-06-03 13:19:24 +0700

    add: victuals edit and create page

---
+++ app/victuals/create/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import arrow from '@/public/svgs/arrow.svg'
+
+export default function CreateVictuals() {
+    return (
+        <div className="w-11/12 mx-auto">
+            <div className="mt-10 flex justify-between">
+                <Link href='/'>
+                    <Image src={arrow} width={30} height={30} alt='icon' />
+                </Link>
+                <p className="font-semibold text-2xl">Create New Victual</p>
+            </div>
+
+            {/* TODO: Create a form here by following the format in "/profile/edit" */}
+
+        </div>
+    )
+}
