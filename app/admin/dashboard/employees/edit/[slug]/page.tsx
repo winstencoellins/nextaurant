cbc52cb4610cc5a236f21b94bab2345098079aa6
WINSTEN COELLINS <62231201@wil.local>
2024-06-10 12:18:31 +0700

    modify: employee api

--- app/admin/dashboard/employees/edit/[slug]/page.tsx
+++ app/admin/dashboard/employees/edit/[slug]/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
 import Link from "next/link"
+import { useState } from "react"
 
 export default function EditEmployee ({ params }: { params: { slug: string } }) {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+
     return (
         <div className="mx-5 mt-8 w-full">
             <h1 className="text-2xl font-semibold">Edit {params.slug}</h1>
@@ -10,22 +18,22 @@ export default function EditEmployee ({ params }: { params: { slug: string } }) 
                     <div className="grid grid-cols-2 gap-x-5 mb-5">
                         <div className="flex flex-col">
                             <label className="mb-1 text-sm">First Name</label>
-                            <input type='text' name='firstName' placeholder='John' className="px-3 py-1.5 rounded-lg border border-slate-300 text-sm" />
+                            <input type='text' name='firstName' value={firstName} onChange={(event) => setFirstName(event.target.value)} placeholder='John' className="px-3 py-1.5 rounded-lg border border-slate-300 text-sm" />
                         </div>
                         <div className="flex flex-col">
                             <label className="mb-1 text-sm">Last Name</label>
-                            <input type='text' name='lastName' placeholder='Doe' className="px-3 py-1.5 rounded-lg border border-slate-300 text-sm" />
+                            <input type='text' name='lastName' value={lastName} onChange={(event) => setLastName(event.target.value)} placeholder='Doe' className="px-3 py-1.5 rounded-lg border border-slate-300 text-sm" />
                         </div>
                     </div>
 
                     <div className="flex flex-col mb-5">
                         <label className="mb-1 text-sm">Username</label>
-                        <input type='text' placeholder='johndoe' className="px-3 py-2 border border-slate-300 rounded-lg text-sm" />
+                        <input type='text' value={username} onChange={(event) => setUsername(event.target.value)} placeholder='johndoe' className="px-3 py-2 border border-slate-300 rounded-lg text-sm" />
                     </div>
 
                     <div className="flex flex-col mb-5">
                         <label className="mb-1 text-sm">Email</label>
-                        <input type='email' placeholder='johndoe@example.com' className="px-3 py-2 border border-slate-300 rounded-lg text-sm" />
+                        <input type='email' value={email} onChange={(event) => setEmail(event.target.value)} placeholder='johndoe@example.com' className="px-3 py-2 border border-slate-300 rounded-lg text-sm" />
                     </div>
 
                     <div className="flex flex-col mb-5">
--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -1,9 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db";
 
 export async function POST(req: NextRequest, res: NextResponse) {
     const data = await req.formData();
 
-    console.log(data)
+    const firstName:any = data.get('firstName')
+    const lastName:any = data.get('lastName')
+    const username:any = data.get('username')
+    const password:any = data.get('password')
+    const email:any = data.get('email')
+
+    const existUser = prisma.user.findUnique({
+        where: {
+            username: username,
+        }
+    })
+
+    console.log('Exist User', existUser)
+    // const user = prisma.user.create({
+    //     data: {
+    //         firstName: firstName,
+    //         lastName: lastName,
+    //         username: username,
+    //         email: email,
+    //         password: password,
+    //     }
+    // })
 
     return NextResponse.json({ "success": true, "message": "Successfully created a user" })
 }
