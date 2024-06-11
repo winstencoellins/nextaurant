d90df478f78f2c477c059aa666c16ec155809222
Vinix <Vinix.Collen@student.uts.edu.au>
2024-06-11 15:18:36 +1000

    edit admin/dashboard page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,16 +1,18 @@
 export default function Dashboard() {
     function todayDate(){
-        Date.prototype.getDay()
-        Date.prototype.getDate()
-        Date.prototype.getMonth()
-        Date.prototype.getFullYear()
+        // let today = (new Date()).toString().split(' ').splice(0,4).join(' ')
+        // return today
+        let today = new Date()
+        const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        return dayNames[today.getDay()] + ", " + today.getDate() + " " + monthNames[today.getMonth()] + " " + today.getFullYear()
     }
     return (
         <div className="mx-5 mt-8 w-full">
             <h1 className="text-2xl font-semibold">Dashboard</h1>
 
             <div className="mt-3">
-                <h3>Welcome, Vinix <span className="float-end">Mon, 10 Jun 2024</span></h3>
+                <h3>Welcome, Vinix <span className="float-end">{todayDate()}</span></h3>
             </div>
 
             <div className="grid grid-cols-2 gap-3 mt-3">
