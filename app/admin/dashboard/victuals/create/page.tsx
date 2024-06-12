f00d3ce176d73271afcdc7b9f24a80ba696b7092
WINSTEN COELLINS <62231201@wil.local>
2024-06-12 11:07:57 +0700

    add: create victuals admin validation

--- app/admin/dashboard/victuals/create/page.tsx
+++ app/admin/dashboard/victuals/create/page.tsx
@@ -6,8 +6,16 @@ import dollar from '@/public/svgs/dollar.svg'
 import Link from "next/link"
 import { FormEvent } from "react"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
+import clsx from "clsx"
 
 export default function CreateVictuals() {
+    const [valid, setValid] = useState(true)
+    const [validName, setValidName] = useState(true)
+    const [validShort, setValidShort] = useState(true)
+    const [validDesc, setValidDesc] = useState(true)
+    const [validPrice, setValidPrice] = useState(true)
+
     const router = useRouter()
     
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
@@ -15,18 +23,92 @@ export default function CreateVictuals() {
 
         const formData = new FormData(event.currentTarget)
 
-        const res = await fetch('/api/victuals', {
-            method: 'POST',
-            body: formData,
-        })
+        // Handles the validation of the form
+        let valid = formValidation(formData)
 
-        const data = await res.json()
-
-        console.log(data.message)
-
-        if (data.success) {
-            return router.push('/admin/dashboard/victuals')
+        if (valid) {
+            const res = await fetch('/api/victuals', {
+                method: 'POST',
+                body: formData,
+            })
+    
+            const data = await res.json()
+    
+            console.log(data.message)
+    
+            if (data.success) {
+                return router.push('/admin/dashboard/victuals')
+            }
         }
+    }
+
+    /**
+     * Handles the validation of the formData based on user
+     * input. It checks name, short description, description, and
+     * price
+     * @param formData 
+     * @returns boolean
+     */
+    const formValidation = (formData: any) => {
+        const [name, short, desc, price]: [any, any, any, any] = [formData.get('name'), formData.get('short'), formData.get('desc'), formData.get('price')]
+        const [nameErrMsg, shortErrMsg, descErrMsg, priceErrMsg]: [any, any, any, any] = [document.getElementById('name-err-msg'), document.getElementById('short-err-msg'), document.getElementById('desc-err-msg'), document.getElementById('price-err-msg')]
+        let valid = true
+        let priceRegex = /^(0|[1-9]\d*)(\.\d+)?$/g
+
+        if (name === "") {
+            nameErrMsg.innerHTML = "Can't be empty."
+            valid = false
+
+            setValidName(false)
+        } else {
+            nameErrMsg.innerHTML = ""
+            valid = true
+
+            setValidName(true)
+        }
+
+        if (short === '') {
+            shortErrMsg.innerHTML = "Can't be empty."
+            valid = false
+
+            setValidShort(false)
+        } else {
+            shortErrMsg.innerHTML = ""
+            valid = true
+
+            setValidShort(true)
+        }
+
+        if (desc === '') {
+            descErrMsg.innerHTML = "Can't be empty."
+            valid = false
+            
+            setValidDesc(false)
+        } else {
+            descErrMsg.innerHTML = ""
+            valid = true
+
+            setValidDesc(true)
+        }
+
+        if (price === '') {
+            priceErrMsg.innerHTML = "Can't be empty."
+            valid = false
+
+            setValidPrice(false)
+        } else if (price.match(priceRegex) === null) {
+            priceErrMsg.innerHTML = 'Please enter a valid amount with two decimal precision.'
+            valid = false
+
+            setValidPrice(false)
+        } else {
+            priceErrMsg.innerHTML = ""
+            valid = true
+
+            setValidPrice(true)
+        }
+
+        return valid
     }
 
     return (
@@ -38,7 +120,8 @@ export default function CreateVictuals() {
                     <div className="grid grid-cols-2 gap-x-5 mb-5">
                         <div className="flex flex-col">
                             <label className="mb-1 text-sm">Name</label>
-                            <input type='text' name="name" placeholder='Fried Rice' className="px-3 py-1.5 rounded-lg border border-slate-300 text-sm" />
+                            <input type='text' name="name" placeholder='Fried Rice' className={clsx("px-3 py-1.5 rounded-lg text-sm border", validName ? "border-slate-300" : "border-red-500")} />
+                            <p className="text-red-500 text-xs mt-1" id='name-err-msg'></p>
                         </div>
                         <div className="flex flex-col">
                             <label className="mb-1 text-sm">Category</label>
@@ -52,19 +135,22 @@ export default function CreateVictuals() {
 
                     <div className="flex flex-col mb-5">
                         <label className="mb-1 text-sm">Short Description</label>
-                        <textarea name='short' placeholder='Write a short description ...' className="px-3 py-2 border border-slate-300 rounded-lg text-sm" />
+                        <textarea name='short' placeholder='Write a short description ...' className={clsx("px-3 py-2 border rounded-lg text-sm", validShort ? "border-slate-300" : "border-red-500")} />
+                        <p className="text-red-500 text-xs mt-1" id="short-err-msg"></p>
                     </div>
 
                     <div className="flex flex-col mb-5">
                         <label className="mb-1 text-sm">Description</label>
-                        <textarea name='desc' placeholder='Write a description ...' className="px-3 py-2 border border-slate-300 rounded-lg text-sm" />
+                        <textarea name='desc' placeholder='Write a description ...' className={clsx("px-3 py-2 border rounded-lg text-sm", validDesc ? "border-slate-300" : "border-red-500")} />
+                        <p className="text-red-500 text-xs mt-1" id="desc-err-msg"></p>
                     </div>
 
                     <div className="flex flex-col mb-10">
                         <label className="mb-1 text-sm">Price</label>
                         <div className="relative">
                             <Image src={dollar} width={20} height={20} alt='icon' className="absolute top-2 left-1" />
-                            <input type='text' name='price' placeholder='10' className="px-3 py-2 border border-slate-300 rounded-lg text-sm px-8 py-2 w-full" />
+                            <input type='text' name='price' placeholder='10' className={clsx("px-3 py-2 border rounded-lg text-sm px-8 py-2 w-full", validPrice ? "border-slate-300": "border-red-500")} />
+                            <p className="text-red-500 text-xs mt-1" id="price-err-msg"></p>
                         </div>
                     </div>
 
--- app/api/victuals/route.ts
+++ app/api/victuals/route.ts
@@ -6,12 +6,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     let [name, short, desc, category, price]: [any, any, any, any, any] = [data.get('name'), data.get('short'), data.get('desc'), data.get('category'), data.get('price')]
 
-    console.log(name, typeof(name))
-    console.log(short, typeof(short))
-    console.log(desc, typeof(desc))
-    console.log(category, typeof(category))
-    console.log(price, typeof(parseFloat(price)))
-
     const createVictual = await prisma.victual.create({
         data: {
             name: name,
