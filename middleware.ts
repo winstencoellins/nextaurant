60d66e66238ff78975c61aec93cd543e7af373c6
WINSTEN COELLINS <62231201@wil.local>
2024-06-20 11:49:01 +0700

    modify: middleware access

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    return NextResponse.redirect(new URL('/login', req.url))
+    return NextResponse.next()
 }
 
 export const config = {
