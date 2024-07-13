db896e0d9604f814ff1aec0aa829cb13e587f16a
winstencoellins <winstencoellinss@gmail.com>
2024-07-13 14:00:00 +0700

    modify: disable middleware

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,19 @@ import { cookies } from "next/headers";
 import { decrypt } from "@/auth";
 
 export async function middleware(req: NextRequest) {
-    try {
-        const valid = await decrypt(cookies().get('session')?.value)
+    // try {
+    //     const valid = await decrypt(cookies().get('session')?.value)
 
-        if (req.nextUrl.pathname.startsWith('/admin') && cookies().get('role')?.value != 'Admin') {
-            return NextResponse.redirect(new URL('/', req.url))
-        }
+    //     if (req.nextUrl.pathname.startsWith('/admin') && cookies().get('role')?.value != 'Admin') {
+    //         return NextResponse.redirect(new URL('/', req.url))
+    //     }
 
-        return NextResponse.next()
-    } catch (e) {
-        return NextResponse.redirect(new URL('/login', req.url))
-    }
+    //     return NextResponse.next()
+    // } catch (e) {
+    //     return NextResponse.redirect(new URL('/login', req.url))
+    // }
+
+    return NextResponse.next()
 }
 
 export const config = {
